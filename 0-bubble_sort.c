fcbde0b3c83cbf80fa43b86a37a8632e8b5426fa
Sphesihle-Duma <dumasphesihle22@gmail.com>
2023-07-22 12:12:30 -0700

    Added bubble_sort.c

---
+++ 0-bubble_sort.c
@@ -0,0 +1,27 @@
+#include "sort.h"
+/**
+ * bubble_sort - sorts array by a bubble sort algorithm
+ * @array: An array to be sorted
+ * @size: size of the array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, temp, j;
+
+	if (array && size > 2)
+	{
+		for (i = 0; i < size - 1; i++)
+		{
+			for (j = 0; j < size - i - 1; j++)
+			{
+				if (array[j] > array[j + 1])
+				{
+					temp = array[j];
+					array[j] = array[j + 1];
+					array[j + 1] = temp;
+					print_array(array, size);
+				}
+			}
+		}
+	}
+}
